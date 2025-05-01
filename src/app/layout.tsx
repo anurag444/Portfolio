dc122d658c873cb17a94863e452785e7cbddb640
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-05-01 10:48:34 -0400

    Icon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Anurag Teckchandani",
+  description: "Full Stack Engineer with 3+ years of Experience",
 };
 
 export default function RootLayout({
